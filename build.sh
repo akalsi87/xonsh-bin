d600b46aaca9bfe42ca56135592baa2ae6daf40f
Kalsi, Aaditya <akalsi@pm.me>
2025-04-17 22:50:50 -0400

    bump6

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 tag=0.19.4
 version=3.10
-build_version=1
+build_num=1
 url=https://github.com/scc-tw/standalone-python/releases/download/release-2024-04-29/release-${version}-x86_64.tar.gz
 
 mkdir -p .python/
@@ -39,7 +39,7 @@ EOF
 chmod +x xon.sh
 
 # create a custom tar file with maximum compression level
-tar_file="xonsh-${tag}-py-${version}-x86_64-${build_version}.tar.gz"
+tar_file="xonsh-${tag}-py-${version}-x86_64-${build_num}.tar.gz"
 GZIP=-9 tar -czf $tar_file xon.sh .python
 if [ $? -ne 0 ]; then
   echo "Failed to create tar file"
